6b12752e217fa17a433f39906811f8435bb8e0ed
danbi01 <twobi01@gmail.com>
2024-12-22 02:02:07 +0900

    기본 파일들 업로드

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("How fast should the texture scroll?")]
+    public float scrollSpeed;
+
+    [Header("References")]
+    public MeshRenderer meshRenderer;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
